ac450b22317e0cc7538ba543b61ee79836959662
manasseh ibrahim <imanasseh03@gmail.com>
2026-08-01 12:53:14 +0100

    Enhance seat fetching logic; include seat status in response and optimize SQL query

--- transport-booking-system/server/controllers/seatController.js
+++ transport-booking-system/server/controllers/seatController.js
@@ -2,31 +2,47 @@ import pool from "../config/db.js";
 
 export const getSeatsByBus = async (req, res) => {
     try {
+
         const { busId } = req.params;
+
 
         const result = await pool.query(
             `
             SELECT
-                id,
-                seat_number
+                seats.id,
+                seats.seat_number,
+                CASE
+                    WHEN bookings.id IS NOT NULL THEN 'Booked'
+                    ELSE 'Available'
+                END AS status
+
             FROM seats
-            WHERE bus_id = $1
-            ORDER BY seat_number;
+
+            LEFT JOIN bookings
+            ON seats.id = bookings.seat_id
+
+            WHERE seats.bus_id = $1
+
+            ORDER BY seats.seat_number;
             `,
             [busId]
         );
+
 
         res.json({
             success: true,
             seats: result.rows,
         });
 
+
     } catch (error) {
+
         console.error(error);
 
         res.status(500).json({
             success: false,
             message: "Failed to fetch seats",
         });
+
     }
 };
